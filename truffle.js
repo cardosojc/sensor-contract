92637a6c0907b234ae7efe476a2e3ef64ead6235
cardosojc <jose.cs.cardoso@gmail.com>
2017-12-12 00:13:30 +0000

    removed account passwd

--- truffle.js
+++ truffle.js
@@ -9,4 +9,4 @@ module.exports = {
   }
 };
 
-//web3.personal.unlockAccount('0xfE19F4197FFE5c2A1C1D7f051c77637959d8FFc7','Cardoso.1985');
+//web3.personal.unlockAccount('0xfE19F4197FFE5c2A1C1D7f051c77637959d8FFc7','#####');
